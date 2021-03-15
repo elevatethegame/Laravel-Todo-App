3ee32de6c55ca601b69767c7503165c93ecab37d
Bruce Ong <bruceskellator@hotmail.com>
2021-03-15 15:32:27 +0800

    First Commit

---
+++ app/Models/TodoList.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TodoList extends Model
+{
+    use HasFactory;
+
+    public function list_items() {
+        return $this->hasMany(ListItem::class);
+    }
+}
---
+++ database/migrations/2021_03_15_070735_create_list_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->mediumText('description');
+            $table->boolean('is_completed')->default(false);
+            $table->foreignId('todo_list_id')->constrained('todo_lists');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_items');
+    }
+}
