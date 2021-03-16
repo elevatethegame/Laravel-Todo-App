039812de49a71a5eab792bb7c37e4a76e7684d13
Bruce Ong <bruceskellator@hotmail.com>
2021-03-16 10:18:48 +0800

    Changed main index page for authenticated user.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,13 @@
         <h1>Welcome to the Laravel Todo App</h1>
         <h5 class="mt-4">This is a simple Laravel App to help keep track of your daily tasks!</p>
         <p class="mt-4">
-            <a class="btn btn-primary btn-lg mr-2" href="/login" role="button">Login</a> 
-            <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+            @auth
+                <a class="btn btn-primary btn-lg" href="/home" role="button">Back to Dashboard</a>
+            @endauth
+            @guest
+                <a class="btn btn-primary btn-lg mr-2" href="/login" role="button">Login</a> 
+                <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+            @endguest
         </p>
     </div>
 @endsection
